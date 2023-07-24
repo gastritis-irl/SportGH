507474b2a2c869e3337c85d66c1c3cbe60658123
Biro Akos <biroakos0623@gmail.com>
2023-07-24 12:43:30 +0300

    Removed unnecessary import from app-routing

--- src/main/webapp/src/app/app-routing.module.ts
+++ src/main/webapp/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {UserComponent} from "./user/user.component";
-import {AppComponent} from "./app.component";
 
 const routes: Routes = [
     {
