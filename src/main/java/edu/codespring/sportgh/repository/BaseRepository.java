2218b77d3f93f69cbeb5ad5c03630d6b91c2f149
Biro Akos <biroakos0623@gmail.com>
2023-08-02 09:41:16 +0300

    [sgh-12-30] Specified T type in BaseRepository

--- src/main/java/edu/codespring/sportgh/repository/BaseRepository.java
+++ src/main/java/edu/codespring/sportgh/repository/BaseRepository.java
@@ -1,10 +1,11 @@
 package edu.codespring.sportgh.repository;
 
+import edu.codespring.sportgh.model.BaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
-public interface BaseRepository<T> extends JpaRepository<T, Long> {
+public interface BaseRepository<T extends BaseEntity> extends JpaRepository<T, Long> {
 
     T findByName(String name);
 
