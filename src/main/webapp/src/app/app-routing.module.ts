0e80939b9a612f8dabaa0fba07107403b2722250
Biro Akos <biroakos0623@gmail.com>
2023-08-02 10:02:42 +0300

    [sgh-12-30] Removed unused imports

--- src/main/webapp/src/app/app-routing.module.ts
+++ src/main/webapp/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
-import { ProductListComponent } from "./product/product-list/product-list.component";
-import { ProductDetailsComponent } from './product/product-details/product-details.component';
 
 const routes: Routes = [
     {
