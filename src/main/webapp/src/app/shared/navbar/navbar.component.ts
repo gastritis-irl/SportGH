c5bd2e10d52555b92fd5e2e5938dc25f07bfca54
Biro Akos <biroakos0623@gmail.com>
2023-08-02 10:05:40 +0300

    [sgh-12-30] Changed hrefs to routerLink

--- src/main/webapp/src/app/shared/navbar/navbar.component.ts
+++ src/main/webapp/src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NgbDropdownModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { OffcanvasComponent } from "../offcanvas/offcanvas.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'sgh-navbar',
     standalone: true,
-    imports: [NgbNavModule, NgbDropdownModule, OffcanvasComponent],
+    imports: [NgbNavModule, NgbDropdownModule, OffcanvasComponent, RouterLink],
     templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
