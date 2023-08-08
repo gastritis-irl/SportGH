92728791fc471ff6a7d3357919396ea313b55168
Biro Akos <biroakos0623@gmail.com>
2023-08-08 14:28:21 +0300

    [SGH-38] Added category list to admin/categories page

--- src/main/webapp/src/app/admin/admin.component.ts
+++ src/main/webapp/src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CategoryService } from '../category/category.service';
+import { Category } from '../category/category.model';
 
 @Component({
     selector: 'sgh-admin',
@@ -6,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-    constructor() {
+    @Input() categories: Category[] = [];
+
+    constructor(private categoryService: CategoryService) {
     }
 
     ngOnInit(): void {
+        this.categoryService.getAll().subscribe(
+            {
+                next: (data: Category[]): void => {
+                    this.categories = data;
+                },
+                error: (error): void => {
+                    console.error('Error fetching data (categories):', error);
+                }
+            }
+        );
     }
 }
