9a1a136aaf76d26621491d472d5eff9fb9b6ac40
CsaB <b.f.csabesz@gmail.com>
2023-08-08 13:27:51 +0300

    [SGH-11][SGH-16] Updated logging.

--- src/main/java/edu/codespring/sportgh/config/FirebaseConfig.java
+++ src/main/java/edu/codespring/sportgh/config/FirebaseConfig.java
@@ -38,8 +38,9 @@ public class FirebaseConfig {
                 FirebaseApp.initializeApp(options);
             }
         } catch (IOException e) {
-            log.error("Could not open serviceAccountKey", e);
-            throw new IllegalStateException("Could not open serviceAccountKey", e);
+            log.error("Could not open serviceAccountKey\n" +
+                "Set the environment variable for the file path to the file containing the ServiceAccountKey.", e);
+            throw new IllegalStateException("Could not open serviceAccountKey file", e);
         }
     }
 }
