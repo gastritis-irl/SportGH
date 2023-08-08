5a9886874120cb2cbfff3e9a624f1eafe5466912
Biro Akos <biroakos0623@gmail.com>
2023-08-08 15:01:16 +0300

    [SGH-43] css

--- src/main/webapp/src/app/shared/navbar/navbar.component.ts
+++ src/main/webapp/src/app/shared/navbar/navbar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NgbDropdownModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { OffcanvasComponent } from "../offcanvas/offcanvas.component";
 import { RouterLink } from '@angular/router';
+import { NgForOf } from '@angular/common';
 
 @Component({
     selector: 'sgh-navbar',
     standalone: true,
-    imports: [NgbNavModule, NgbDropdownModule, OffcanvasComponent, RouterLink],
+    imports: [NgbNavModule, NgbDropdownModule, OffcanvasComponent, RouterLink, NgForOf],
     templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
