f5ba0097e3c180692477a344f4b77a8f9bb90123
CsaB <b.f.csabesz@gmail.com>
2023-08-10 16:01:55 +0300

    [SGH-11][SGH-16] Deprecated subscribe resolved.

--- src/main/webapp/src/app/authentication/authentication.component.ts
+++ src/main/webapp/src/app/authentication/authentication.component.ts
@@ -3,11 +3,12 @@ import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 import { UserService } from '../user/user.service';
 import { Router } from '@angular/router';
 
+    
 @Component({
     selector: 'sgh-authentication',
     templateUrl: './authentication.component.html',
 })
-
+    
 export class AuthenticationComponent {
 
     @ViewChild('loginContent') loginContent!: TemplateRef<string>;
@@ -35,44 +36,44 @@ export class AuthenticationComponent {
     }
 
     login(): void {
-    this.userService.signinWithFirebase(this.email, this.password).then(userObservable => {
-        userObservable.subscribe(
-            () => {
-                this.loggedInUserEmail = this.email; // Store the logged-in email
-                this.closeOffcanvas();
-                this.router.navigate(['/home']);
-            },
-            error => {
-                console.error('Login failed', error);
-                alert('Login failed' + error.message);
-                this.errorMessage = 'Login failed. Please try again.';
-            }
-        );
-    }).catch(error => {
-        console.error('Error in Firebase authentication', error);
-        alert('Login failed' + error.message);
-        this.errorMessage = 'An error occurred during login. Please try again later.';
-    });
-}
-
+        this.userService.signinWithFirebase(this.email, this.password).then(userObservable => {
+            userObservable.subscribe({
+                next: () => {
+                    this.loggedInUserEmail = this.email; // Store the logged-in email
+                    this.closeOffcanvas();
+                    this.router.navigate(['/home']);
+                },
+                error: (error) => {
+                    console.error('Login failed', error);
+                    alert('Login failed' + error.message);
+                    this.errorMessage = 'Login failed. Please try again.';
+                }
+            });
+        }).catch(error => {
+            console.error('Error in Firebase authentication', error);
+            alert('Login failed' + error.message);
+            this.errorMessage = 'An error occurred during login. Please try again later.';
+        });
+    }
 
     register(): void {
         this.userService.registerWithFirebase(this.email, this.password).then(userObservable => {
-        userObservable.subscribe(
-            () => {
-                alert('Registration successful');
-                this.openOffcanvas(this.loginContent);
-            },
-            error => {
-                console.error('Registration failed', error);
-                alert('Registration failed' + error.message);
-                this.errorMessage = 'Registration failed. Please try again.';
-            }
-        );
+            userObservable.subscribe({
+                next: () => {
+                    alert('Registration successful');
+                    this.openOffcanvas(this.loginContent);
+                },
+                error: (error) => {
+                    console.error('Registration failed', error);
+                    alert('Registration failed' + error.message);
+                    this.errorMessage = 'Registration failed. Please try again.';
+                }
+            });
         }).catch(error => {
             console.error('Error in Firebase registration', error);
             alert('Registration failed.\n' + error.message);
             this.errorMessage = 'An error occurred during registration. Please try again later.';
         });
     }
+
 }
