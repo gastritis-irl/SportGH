ad5c9fab421c4c848759c9911bab23393d51585e
Biro Akos <biroakos0623@gmail.com>
2023-08-11 10:38:32 +0300

    [SGH-43] Removed SharedModule

--- src/main/webapp/src/app/shared/navbar/navbar.component.ts
+++ src/main/webapp/src/app/shared/navbar/navbar.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NgbDropdownModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterLink } from '@angular/router';
-import { SharedModule } from '../shared.module';
 import { AuthModule } from '../../authentication/authentication.module';
 
 import { NgForOf, NgIf } from '@angular/common';
@@ -9,7 +8,7 @@ import { NgForOf, NgIf } from '@angular/common';
 @Component({
     selector: 'sgh-navbar',
     standalone: true,
-    imports: [NgbNavModule, NgbDropdownModule, RouterLink, SharedModule, AuthModule, NgForOf, NgIf],
+    imports: [NgbNavModule, NgbDropdownModule, RouterLink, AuthModule, NgForOf, NgIf],
     templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
