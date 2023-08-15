fc9d473672cc7011375343cdaeae4aad05f6c196
Biro Akos <biroakos0623@gmail.com>
2023-08-15 14:52:56 +0300

    [SGH-43] Removed unnecessary variables; minor style changes

--- src/main/webapp/src/app/shared/navbar/navbar.component.ts
+++ src/main/webapp/src/app/shared/navbar/navbar.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { NgbDropdownModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterLink } from '@angular/router';
 import { AuthModule } from '../../authentication/authentication.module';
 
-import { NgForOf, NgIf } from '@angular/common';
+import { NgForOf } from '@angular/common';
 
 @Component({
     selector: 'sgh-navbar',
     standalone: true,
-    imports: [NgbNavModule, NgbDropdownModule, RouterLink, AuthModule, NgForOf, NgIf],
+    imports: [NgbDropdownModule, RouterLink, AuthModule, NgForOf],
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.scss'],
 })
