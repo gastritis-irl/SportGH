2c17fbe4e94aec2147774d88588d58ee3e81531b
Biro Akos <biroakos0623@gmail.com>
2023-08-16 14:14:35 +0300

    [SGH-43] Category list added to navbar

--- src/main/webapp/src/app/shared/navbar/navbar.component.ts
+++ src/main/webapp/src/app/shared/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterLink } from '@angular/router';
 import { AuthModule } from '../../authentication/authentication.module';
 
 import { NgForOf } from '@angular/common';
+import { CategoryService } from '../../category/category.service';
+import { Category } from '../../category/category.model';
 
 @Component({
     selector: 'sgh-navbar',
@@ -12,5 +14,23 @@ import { NgForOf } from '@angular/common';
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+    categories: Category[] = [];
+
+    constructor(private categoryService: CategoryService) {
+    }
+
+    ngOnInit(): void {
+        this.categoryService.getAll().subscribe(
+            {
+                next: (data: Category[]): void => {
+                    this.categories = data;
+                },
+                error: (error): void => {
+                    console.error(error);
+                }
+            }
+        );
+    }
 }
