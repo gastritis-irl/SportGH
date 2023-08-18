72c86061c45a5bfd654cd881896be497b4fba699
Biro Akos <biroakos0623@gmail.com>
2023-08-18 08:18:09 +0300

    [SGH-32] Fixed error; Override handleMethodArgumentNotValid

--- src/main/java/edu/codespring/sportgh/controller/RestResponseEntityExceptionHandler.java
+++ src/main/java/edu/codespring/sportgh/controller/RestResponseEntityExceptionHandler.java
@@ -4,9 +4,11 @@ import edu.codespring.sportgh.service.ServiceException;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.exception.DataException;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,13 +22,15 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({
-        MethodArgumentNotValidException.class,
-    })
-    protected ResponseEntity<Object> handleException(RuntimeException e, WebRequest request) {
-        log.warn(e.getMessage());
-        return handleExceptionInternal(e, e.getMessage(),
-            new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+        MethodArgumentNotValidException ex,
+        @NotNull HttpHeaders headers,
+        @NotNull HttpStatusCode status,
+        @NotNull WebRequest request
+    ) {
+        log.warn(ex.getMessage());
+        return handleExceptionInternal(ex, ex.getMessage(), headers, status, request);
     }
 
     @ExceptionHandler({
