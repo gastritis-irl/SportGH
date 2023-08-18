b8cc1eda45726afdb85324f2a85f5535d18a5dfd
Biro Akos <biroakos0623@gmail.com>
2023-08-18 15:34:23 +0300

    [SGH-46] Fixed pipeline

--- src/main/java/edu/codespring/sportgh/repository/ProductRepository.java
+++ src/main/java/edu/codespring/sportgh/repository/ProductRepository.java
@@ -5,15 +5,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
+
 import edu.codespring.sportgh.model.User;
 
 public interface ProductRepository extends BaseRepository<Product> {
 
     @Query(
-        "select p from Product p" +
-            " join SubCategory sc on sc=p.subCategory" +
-            " join Category c on c=sc.category" +
-            " where c.id=:categoryId"
+            "select p from Product p"
+                    + " join SubCategory sc on sc=p.subCategory"
+                    + " join Category c on c=sc.category"
+                    + " where c.id=:categoryId"
     )
     Collection<Product> findByCategoryId(@Param("categoryId") Long categoryId);
 
