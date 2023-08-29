c2ba0356f66eec6fb7b5cc61de5f5d2e00b7c4eb
CsaB <b.f.csabesz@gmail.com>
2023-08-29 09:44:57 +0300

    [SGH-45] No need for reactive forms.

--- src/main/webapp/src/app/authentication/authentication.module.ts
+++ src/main/webapp/src/app/authentication/authentication.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AuthenticationComponent } from './authentication.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
         AuthenticationComponent
     ],
     imports: [
-        ReactiveFormsModule,
         CommonModule,
         FormsModule
     ],
