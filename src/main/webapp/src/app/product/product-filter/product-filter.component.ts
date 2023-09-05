29ad149187cf8c2679c3498d85839d5783046107
Biro Akos <biroakos0623@gmail.com>
2023-09-05 09:46:07 +0300

    [SGH-34] Filter style changes

--- src/main/webapp/src/app/product/product-filter/product-filter.component.ts
+++ src/main/webapp/src/app/product/product-filter/product-filter.component.ts
@@ -7,12 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductFilterComponent implements OnInit {
 
-    nrOfFilters: number = 5;
+    nrOfFilters: number = 0;
     isCollapsed: boolean[] = [];
 
     constructor() {
     }
 
     ngOnInit(): void {
+        this.getDefaultValues();
+        this.setDefaultValues();
+    }
+
+    getDefaultValues(): void {
+        this.nrOfFilters = 5;
+    }
+
+    setDefaultValues(): void {
+        // collapse all filters
+        for (let i: number = 0; i < this.nrOfFilters; i++) {
+            this.isCollapsed[i] = true;
+        }
+    }
+
+    range(from: number, to: number): number[] {
+        if (from >= to) {
+            return [];
+        }
+        const result: number[] = [];
+        for (let i: number = from; i < to; i++) {
+            result.push(i);
+        }
+        return result;
     }
 }
