f56ed4b151a5b544ed966ee4de9b040ed3b1643f
CsaB <b.f.csabesz@gmail.com>
2023-09-07 11:05:53 +0300

    [SGH-13] hmm.

--- src/main/webapp/src/app/authentication/authentication.module.ts
+++ src/main/webapp/src/app/authentication/authentication.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AuthenticationComponent } from './authentication.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [
@@ -9,7 +10,13 @@ import { AuthenticationComponent } from './authentication.component';
     ],
     imports: [
         CommonModule,
-        FormsModule
+        FormsModule,
+        RouterModule.forChild([
+            {
+                path: '',
+                component: AuthenticationComponent
+            }
+        ])
     ],
     exports: [
         AuthenticationComponent // If you want to use it in other modules
