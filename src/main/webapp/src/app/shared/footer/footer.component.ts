58c3e02c01782f86bcdb7d2638570191076b2e53
Biro Akos <biroakos0623@gmail.com>
2023-09-13 14:46:38 +0300

    [SGH-66] Added icons from Figma design; style changes (gap/align-item changes); added description (instead of lorem ipsum); added frame

--- src/main/webapp/src/app/shared/footer/footer.component.ts
+++ src/main/webapp/src/app/shared/footer/footer.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'sgh-footer',
     standalone: true,
-    imports: [],
+    imports: [
+        RouterLink
+    ],
     templateUrl: './footer.component.html',
     styleUrls: [ './footer.component.scss' ],
 })
