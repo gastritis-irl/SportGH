90eb581b5def0978009bd2a8c5e2520cc8e8d35f
Biro Akos <biroakos0623@gmail.com>
2023-09-18 15:30:50 +0300

    [SGH-41] Backend base for rent requests done

---
+++ src/main/java/edu/codespring/sportgh/controller/RentController.java
@@ -0,0 +1,13 @@
+package edu.codespring.sportgh.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/rent")
+@RequiredArgsConstructor
+@Slf4j
+public class RentController {
+
+}
---
+++ src/main/java/edu/codespring/sportgh/repository/RentRepository.java
@@ -0,0 +1,7 @@
+package edu.codespring.sportgh.repository;
+
+import edu.codespring.sportgh.model.RentRequest;
+
+public interface RentRepository extends BaseRepository<RentRequest> {
+
+}
