57b9042e3d51a2ec255dd89ac8aca360c1cad90e
CsaB <b.f.csabesz@gmail.com>
2023-09-22 12:01:29 +0300

    [SGH-13] MR threads.

--- src/main/java/edu/codespring/sportgh/dto/UserInDTO.java
+++ src/main/java/edu/codespring/sportgh/dto/UserInDTO.java
@@ -24,13 +24,13 @@ public class UserInDTO {
     @NotNull(message = "FirebaseUid can't be empty")
     private String firebaseUid;
 
-    @Size(max = 30,  message = "Full name must be less than 30 characters")
+    @Size(max = 30, message = "Full name must be less than 30 characters")
     private String fullName;
 
     @Size(min = 9, max = 12, message = "Phone number should be between 9 and 12 characters")
     private String phoneNumber;
 
-    @Size(max=2000, message = "Bio must be under 2000 characters")
+    @Size(max = 2000, message = "Bio must be under 2000 characters")
     private String bio;
 
     @Size(min = 5, max = 80, message = "Address should be between 5 and 8 characters")
