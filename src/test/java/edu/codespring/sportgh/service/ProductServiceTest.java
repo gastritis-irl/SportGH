d450297496d7a36ff2abf5766296d53ded82d9fe
Biro Akos <biroakos0623@gmail.com>
2024-06-01 11:24:31 +0300

    [SGH-132] Fixed pipeline (contextLoads error)

--- src/test/java/edu/codespring/sportgh/service/ProductServiceTest.java
+++ src/test/java/edu/codespring/sportgh/service/ProductServiceTest.java
@@ -16,7 +16,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class ProductServiceTest {
+class ProductServiceTest {
 
     @Mock
     private ProductRepository productRepository;
